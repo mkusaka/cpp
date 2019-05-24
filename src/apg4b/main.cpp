b4feb6d90daa422cf7fe5b83161f0b842e964a76
mkusaka <hinoshita1992@gmail.com>
2019-05-25 01:27:30 +0900

    side by side check

--- src/apg4b/main.cpp
+++ src/apg4b/main.cpp
@@ -4,21 +4,18 @@
 using namespace std;
 
 int main() {
-
-  int N;
-  cin >> N;
+  vector<int> a(5);
+  for (int i; i < a.size(); i++) {
+    cin >> a.at(i);
+  }
 
   int cnt = 0;
-  int rest = 0;
-
-  while (N > 0) {
-    if (N % 2 > 0) {
-      rest = N;
-      break;
+  for (int i; i < a.size() - 1; i++) {
+    if (a.at(i) == a.at(i + 1)) {
+      cout << "YES" << endl;
+      return 0;
     }
-
-    N = N / 2;
-    cnt++;
   }
-  cout << N << " は " << cnt << "回2で割れた" << endl << "2で割り切った残りは " << rest << endl;
+  cout << "NO" << endl;
+  return 0;
 }
